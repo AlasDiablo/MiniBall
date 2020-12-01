853184f5c238973a76014212a129ca2189250ef9
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-12-01 16:17:12 +0100

    Add debug and commentary

--- core/src/fr/alasdiablo/miniball/screen/GameScreen.java
+++ core/src/fr/alasdiablo/miniball/screen/GameScreen.java
@@ -18,20 +18,19 @@ public class GameScreen implements Screen {
     private final FitViewport viewport;
     private final SpriteBatch batch;
     private final World world;
+    private final Texture terrain;
+    private final Body playerLeft;
+    private final Sprite playerLeftSprite;
+    private final Body playerRight;
+    private final Sprite playerRightSprite;
+    private final InputProcessor playerInput;
+    private final Box2DDebugRenderer debugRenderer;
+
     private int middleX;
     private int middleY;
 
-    private final Texture terrain;
-
-    private final Body playerLeft;
-    private final Sprite playerLeftSprite;
-
-    private final Body playerRight;
-    private final Sprite playerRightSprite;
-
-    private final InputProcessor playerInput;
-
     public GameScreen() {
+        // Setup camera and view
         this.camera = new OrthographicCamera();
         this.viewport = new FitViewport(102.4f, 76.8f, camera);
         this.viewport.apply();
@@ -45,59 +44,75 @@ public class GameScreen implements Screen {
         this.middleX = (int) this.camera.viewportWidth / 2;
         this.middleY = (int) this.camera.viewportHeight / 2;
 
+        // Create sprite batch
         this.batch = new SpriteBatch();
         this.batch.setProjectionMatrix(camera.combined);
 
+        // Create terrain texture
         this.terrain = new Texture("terrain.png");
 
+        // Create world
         this.world = new World(new Vector2(0, 0), false);
 
+        // Create player shape and fixture
         final CircleShape circleShape = new CircleShape();
         circleShape.setRadius(76.8f / 40f);
-
         final FixtureDef playerFixtureDef = new FixtureDef();
         playerFixtureDef.shape = circleShape;
         playerFixtureDef.density = 1f;
         playerFixtureDef.restitution = .25f;
         playerFixtureDef.friction = 1.5f;
 
+        // Create player left body
         final BodyDef playerLeftBodyDef = new BodyDef();
         playerLeftBodyDef.type = BodyDef.BodyType.DynamicBody;
         playerLeftBodyDef.position.set(10f, 76.8f/2f);
         playerLeftBodyDef.linearDamping = 1.5f;
         this.playerLeft = this.world.createBody(playerLeftBodyDef);
         this.playerLeft.createFixture(playerFixtureDef);
+        // Create player left sprite
         this.playerLeftSprite = new Sprite(new Texture("player_left.png"), 32, 32);
         this.playerLeftSprite.setScale(.125f);
 
+        // Create player right body
         final BodyDef playerRightBodyDef = new BodyDef();
         playerRightBodyDef.type = BodyDef.BodyType.DynamicBody;
         playerRightBodyDef.position.set(80f, 76.8f/2f);
         playerRightBodyDef.linearDamping = 1.5f;
         this.playerRight = this.world.createBody(playerRightBodyDef);
         this.playerRight.createFixture(playerFixtureDef);
+        // Create player right sprite
         this.playerRightSprite = new Sprite(new Texture("player_right.png"), 32, 32);
         this.playerRightSprite.setScale(.125f);
-
+        // Handle input
         this.playerInput = new PlayerInput();
+
+        // ---------------------- box 2d debug ----------------------
+        this.debugRenderer = new Box2DDebugRenderer();
 
     }
 
     @Override
     public void render(float delta) {
+        // set background
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // tick world
         world.step(Gdx.graphics.getDeltaTime(), 1, 1);
 
+        // tick player sprite possition
         this.playerLeftSprite.setPosition(this.playerLeft.getPosition().x - this.playerLeftSprite.getWidth()/2f, this.playerLeft.getPosition().y - this.playerLeftSprite.getHeight()/2f);
         this.playerRightSprite.setPosition(this.playerRight.getPosition().x - this.playerRightSprite.getWidth()/2f, this.playerRight.getPosition().y - this.playerRightSprite.getHeight()/2f);
 
+        // star drawing
         this.batch.begin();
         this.batch.draw(this.terrain, this.middleX - 50, this.middleY - 38, 102.4f, 76.8f);
         this.playerLeftSprite.draw(this.batch);
         this.playerRightSprite.draw(this.batch);
         this.batch.end();
+        // ---------------------- box 2d debug ----------------------
+        this.debugRenderer.render(this.world, this.camera.combined);
     }
 
 
